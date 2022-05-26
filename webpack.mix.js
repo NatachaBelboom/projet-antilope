73f8a710fb44502478b97dca92e91399f7bc5974
NatachaBelboom <natacha.belboom@student.hepl.be>
2022-05-26 11:43:27 +0200

    npx installation

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.setPublicPath('./wp-content/themes/no-air/public')
+    .copyDirectory('wp-content/themes/no-air/resources/fonts', 'wp-content/themes/no-air/public/fonts')
+    .js('wp-content/themes/no-air/resources/js/script.js', 'wp-content/themes/no-air/public/js')
+    .sass('wp-content/themes/no-air/resources/sass/style.scss', 'wp-content/themes/no-air/public/css')
+    .options({
+        processCssUrls: false
+    })
+    /*.browserSync({
+        proxy: 'no-air',
+        notify: false
+    })*/
+    .version();
